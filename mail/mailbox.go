e0ab7e01b7727c58f17aac47e5892d01f2765c78
Takeru KODAMA <zen.en.tonal@gmail.com>
2024-02-27 01:39:51 +0000

    [Change] multi

--- mail/mailbox.go
+++ mail/mailbox.go
@@ -4,27 +4,20 @@ import (
 	"sync"
 )
 
-type Forwarder interface {
-	Forward(env Envelope) error
-}
-
-type Forwarders []Forwarder
-
-func (f Forwarders) Forward(env Envelope) error {
+func multi[T any](fs []func(T) error, arg T) error {
 	var wg sync.WaitGroup
-	wg.Add(len(f))
+	wg.Add(len(fs))
 
-	ec := make(chan error, len(f))
-
-	for _, x := range f {
-		go func(x Forwarder, w *sync.WaitGroup) {
-			ec <- x.Forward(env)
+	ec := make(chan error, len(fs))
+	for _, f := range fs {
+		go func(f func(T) error, w *sync.WaitGroup) {
+			ec <- f(arg)
 			w.Done()
-		}(x, &wg)
+		}(f, &wg)
 	}
-
 	wg.Wait()
 	close(ec)
+
 	for err := range ec {
 		if err != nil {
 			return err
@@ -32,6 +25,20 @@ func (f Forwarders) Forward(env Envelope) error {
 	}
 
 	return nil
+}
+
+type Forwarder interface {
+	Forward(env Envelope) error
+}
+
+type Forwarders []Forwarder
+
+func (f Forwarders) Forward(env Envelope) error {
+	var fs []func(Envelope) error
+	for _, x := range f {
+		fs = append(fs, x.Forward)
+	}
+	return multi[Envelope](fs, env)
 }
 
 type NullForwarder struct{}
@@ -71,17 +78,14 @@ func NewMailbox(filter Filter, forward Forwarder, storage Storage) Mailbox {
 }
 
 func (r Mailbox) Recieve(env Envelope) error {
-	err := r.Validate(env)
-	if err != nil {
+	if err := r.Validate(env); err != nil {
 		return err
 	}
-	// TODO: goroutine
-	err = r.Store(env)
-	if err != nil {
-		return err
+	fs := []func(e Envelope) error{
+		r.Forwarder.Forward,
+		r.Storage.Store,
 	}
-	err = r.Forward(env)
-	if err != nil {
+	if err := multi[Envelope](fs, env); err != nil {
 		return err
 	}
 	return nil
